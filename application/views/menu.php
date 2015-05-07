bc6fcb6b9f38652156e9fbdbe73842a09d2b5979
Glenn Satory <glenn_satory@hotmail.com>
2015-05-07 09:44:00 +0200

    intial setup

---
+++ application/views/menu.php
@@ -0,0 +1,61 @@
+<!-- modal login -->
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+  <!-- modal login -->  
+  
+        <div id="wrapper"> 
+            <!-- start header -->             
+            <header> 
+                <div class="navbar navbar-default navbar-static-top"> 
+                    <div class="container"> 
+                        <div class="navbar-header"> 
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> 
+                                <span class="icon-bar"></span> 
+                                <span class="icon-bar"></span> 
+                                <span class="icon-bar"></span> 
+                            </button>                             
+                            <a class="navbar-brand" href="index.html"><span>Tedx</span>pxl</a> 
+                        </div>                         
+                        <div class="navbar-collapse collapse "> 
+                            <ul class="nav navbar-nav"> 
+                                <li class="active">
+                                    <a href="index.html">Home</a>
+                                </li>                                 
+                                <li> 
+                                    <a href="blog.html">over ons</a>
+                                </li>
+                                <li>
+                                    <a href="portfolio.html">evenementen</a>
+                                </li>                                 
+                                <li>
+                                    <a href="blog.html">forum</a>
+                                </li>                                 
+                                <li>
+                                    <a href="contact.html">Contacteer ons</a>
+                                </li>
+                                <li>
+                                   
+                                    <a data-toggle="modal" href="#myModal"><i class="fa fa-users"></i>aanmelden</a>
+                                        
+                                </li>
+                            </ul>                             
+                        </div>                         
+                    </div>                     
+                </div>                 
+            </header>             
+            <!-- end header -->  
---
+++ application/views/modal.php
@@ -0,0 +1,25 @@
+ </div>
+<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a> 
+        <!-- javascript
+    ================================================== -->         
+        <!-- Placed at the end of the document so the pages load faster -->         
+        <script src="assets/js/jquery.js"></script>         
+        <script src="assets/js/jquery.easing.1.3.js"></script>         
+        <script src="assets/js/bootstrap.min.js"></script>         
+        <script src="assets/js/jquery.fancybox.pack.js"></script>         
+        <script src="assets/js/jquery.fancybox-media.js"></script>         
+        <script src="assets/js/google-code-prettify/prettify.js"></script>         
+        <script src="assets/js/portfolio/jquery.quicksand.js"></script>         
+        <script src="assets/js/portfolio/setting.js"></script>         
+        <script src="assets/js/jquery.flexslider.js"></script>         
+        <script src="assets/js/animate.js"></script>         
+        <script src="assets/js/custom.js"></script>         
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <script type="text/javascript">
+	$('#openBtn').click(function(){
+	$('#myModal').modal({show:true})
+});
+</script>
+    </body>     
+</html>
