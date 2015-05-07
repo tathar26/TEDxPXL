767ffedea06d96120560cb3fc1d192ef668dd09c
revalex <info@revalex.be>
2015-05-07 11:08:37 +0200

    Contact pagina test

---
+++ application/views/contact.php
@@ -0,0 +1,23 @@
+<div class="cta-text"> 
+<form id="ContactFormulier">
+	
+		<label for="naam">Naam:</label><br />
+		<input id="naam" class="input" naam="naam" type="text" value="Jos" size="30" /><br />
+		
+		<label for="naam">Achternaam:</label><br />
+		<input id="achternaam" class="input" naam="achternaam" type="text" value="Trekbos" size="30" /><br />
+		
+		<label for="email">TelefoonNummer:</label><br />
+		<input id="gsm" class="input" naam="gsm" type="text" value="047663919191" size="30" /><br />
+	
+		<label for="email">E-mail:</label><br />
+		<input id="email" class="input" naam="email" type="text" value="Jos@Trekbos.be" size="30" /><br />
+	
+		<label for="bericht">Bericht:</label><br />
+		<textarea id="bericht" class="input" naam="bericht" rows="5" cols="50"></textarea><br />
+	
+		<img src="http://designshack.net/wp-content/uploads/killcaptcha-1.jpg" height="100" width="200"></img>
+		<input id="Verzend" type="submit" value="Verstuur" />
+</form>						
+</div>                                 
+<footer> 
