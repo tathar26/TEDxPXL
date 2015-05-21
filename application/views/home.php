20364baba8755b6e611165c4ad51668c1138129d
revalex <info@revalex.be>
2015-05-21 12:12:34 +0200

    no message

--- application/views/home.php
+++ application/views/home.php
@@ -44,19 +44,10 @@
                                 <div class="cta-text"> 
                                     <h2><span>TEDxPXL</span> About us</h2> 
 									<div>
-									About TEDxUHasselt
-TEDxUHasselt is an independently organized TED conference where speakers from around the world share cutting-edge ideas. During the breaks, attendees connect with each other through various interactive activities.
-
-TEDxUHasselt was founded in 2011 by three Hasselt University students and has remained a student-driven organization ever since. Meet our team members!
-
-About TEDx
-
-About TEDx, x = independently organized event In the spirit of ideas worth spreading, TEDx is a program of local, self-organized events that bring people together to share a TED-like experience. At a TEDx event, TEDTalks video and live speakers combine to spark deep discussion and connection in a small group. These local, self-organized events are branded TEDx, where x = independently organized TED event. The TED Conference provides general guidance for the TEDx program, but individual TEDx events are self-organized. (Subject to certain rules and regulations.)
-
-About TED
-TED is a nonprofit organization devoted to Ideas Worth Spreading. Started as a four-day conference in California 25 years ago, TED has grown to support those world-changing ideas with multiple initiatives. The annual TED Conference invites the world's leading thinkers and doers to speak for 18 minutes. Their talks are then made available, free, at TED.com. TED speakers have included Bill Gates, Al Gore, Jane Goodall, Elizabeth Gilbert, Sir Richard Branson, Nandan Nilekani, Philippe Starck, Ngozi Okonjo-Iweala, Isabel Allende and UK Prime Minister Gordon Brown. The annual TED Conference takes place in Long Beach, California, with simulcast in Palm Springs; TEDGlobal is held each year in Oxford, UK.
-
-TED's media initiatives include TED.com, where new TEDTalks are posted daily, and the Open Translation Project, which provides subtitles and interactive transcripts as well as the ability for any TEDTalk to be translated by volunteers worldwide. TED has established the annual TED Prize, where exceptional individuals with a wish to change the world are given the opportunity to put their wishes into action; TEDx, which offers individuals or groups a way to organize independent, TED-like events around the world; and the TEDFellows program, helping world-changing innovators from around the globe to become part of the TED community and, with its help, amplify the impact of their remarkable projects and activities. Follow TED on Twitter at twitter.com/TEDTalks, or on Facebook at facebook.com/TED.
+									<p>In the spirit of ideas worth spreading, TEDx is a program of local, self-organized events that bring people together to share a TED-like experience.</p> 
+									<p>At a TEDx event, TEDTalks video and live speakers combine to spark deep discussion and connection in a small group.</p>
+									<p>These local, self-organized events are branded TEDx, where x = independently organized TED event.</p> 
+									<p>The TED Conference provides general guidance for the TEDx program, but individual TEDx events are self-organized.</p>
 									</div>
                                 </div>                                 
                             </div>                             
